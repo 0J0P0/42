6c48ad4bd28ac0480b722a4be07a137109c6d730
0J0P0 <juan.pablo.zaldivar@estudiantat.upc.edu>
2023-02-16 20:03:41 +0100

    Mlx Management

---
+++ Fdf/src/ft_mlx.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mlx.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzaldiva <jzaldiva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 19:58:42 by jzaldiva          #+#    #+#             */
+/*   Updated: 2023/02/16 19:58:42 by jzaldiva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../inc/fdf.h"
+
+// Function to initialize mlx.
+// Returns a t_mlx structure.
+t_mlx	ft_mlx_init(void)
+{
+    t_mlx	mlx;
+
+    mlx.mlx = mlx_init();
+    if (!mlx.mlx)
+	 	ft_error(ERR_MLX, 1);
+    mlx.win = mlx_new_window(mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "Fdf");
+    if (!mlx.win)
+        ft_error(ERR_MLX, 1);
+    // mlx.img = mlx_new_image(mlx.mlx, WIN_WIDTH, WIN_HEIGHT);
+    // if (!mlx.img)
+    //     ft_error(ERR_MLX, 1);
+    // mlx.data = (int *)mlx_get_data_addr(mlx.img, &mlx.bpp, &mlx.size_line, &mlx.endian);
+    // if (!mlx.data)
+    //     ft_error(ERR_MLX, 1);
+    return (mlx);
+}
